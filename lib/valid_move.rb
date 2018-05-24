86ee92e14ca77269549c943a0cee9fc133b5d9ce
Wendy Calderon <wendycalderon@gmail.com>
2018-05-24 02:52:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 def valid_move?(board, index)
   if index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8
     true
+  else
+    false
   end
 end
 
